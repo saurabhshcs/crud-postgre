e8a6a0e812ad367ddca004ed631c696e8d9a49ed
Saurabh Sharma <saurabhshcs@gmail.com>
2021-05-28 23:59:41 +0100

    [crud-postgres-1] saurabhshcs - Add necessary files

--- src/main/java/com/techsharezone/crud/postgre/controller/EmployeeController.java
+++ src/main/java/com/techsharezone/crud/postgre/controller/EmployeeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController(value="/employee")
 public class EmployeeController {
 
     @Autowired
